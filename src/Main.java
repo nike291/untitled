bc7a7eb9b7ef790ed868da73d4a95e4fd4ee6324
Николай <nike291@yandex.ru>
2022-05-28 18:26:14 +0500

    1 commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int age = 19;
+        if (age >= 18 && age < 21) {
+            System.out.println("Ты можешь водить, но не можешь пить алкоголь");
+        } else {
+            System.out.println("Ты не можешь водить");
+        }
     }
 }
